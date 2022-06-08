6abce1b7588dd37d41cc3858381ededc3ab01757
starold <bintangp630@gmail.com>
2022-06-08 16:02:13 +0700

    modified

---
+++ back-end/models/report.js
@@ -0,0 +1,11 @@
+class Reports {
+    constructor(id, userReport, titleReport, Report, createdReportby ) {
+            this.id = id;
+            this.userReport = userReport;
+            this.titleReport = titleReport;
+            this.report = report;
+            this.createdReportby = createdReportby;
+    }
+}
+
+module.exports = Report;
